aa506dc5730ed14d9d53488b33d997be7074f10b
JP Dillingham <jpdillingham@gmail.com>
2019-10-01 19:22:11 -0500

    stub register and question endpoints, add static files

--- hub/hub.go
+++ hub/hub.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 
@@ -13,9 +15,22 @@ func question(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	fmt.Fprintf(w, "question: %s, TTL: %s\n", queryValues.Get("q"), queryValues.Get("ttl"))
 }
 
+func register(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576))
+	if err != nil {
+		panic(err)
+	}
+	if err := r.Body.Close(); err != nil {
+		panic(err)
+	}
+
+	fmt.Fprintf(w, "register; auth: %s, body: %s\n", r.Header.Get("Authorization"), body)
+}
+
 func main() {
 	router := httprouter.New()
 	router.GET("/question", question)
+	router.POST("/register", register)
 
 	// if not found look for a static file
 	static := httprouter.New()
