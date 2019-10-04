69958ee0c7df06341035b40d672bd39ebdd19d14
JP Dillingham <jpdillingham@gmail.com>
2019-10-03 22:00:22 -0500

    implement de/registration, stub answer handler

--- hub/hub.go
+++ hub/hub.go
@@ -1,41 +1,84 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"io"
-	"io/ioutil"
 	"log"
 	"net/http"
 
+	model "./model"
 	"github.com/julienschmidt/httprouter"
 )
 
-func question(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	queryValues := r.URL.Query()
-	fmt.Fprintf(w, "question: %s, TTL: %s\n", queryValues.Get("q"), queryValues.Get("ttl"))
-}
+const errorsBeforeDeregistration = 3
+const sharedKey = "Bearer b0486c92-f2ef-487b-9cdf-8550f2177fb5"
 
-func register(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576))
-	if err != nil {
-		panic(err)
-	}
-	if err := r.Body.Close(); err != nil {
-		panic(err)
-	}
-
-	fmt.Fprintf(w, "register; auth: %s, body: %s\n", r.Header.Get("Authorization"), body)
-}
+var agents map[string]int8
 
 func main() {
-	router := httprouter.New()
-	router.GET("/question", question)
-	router.POST("/register", register)
+	agents = map[string]int8{}
 
-	// if not found look for a static file
+	router := httprouter.New()
+	router.GET("/answer", answer)
+	router.PUT("/webhooks/:url", register)
+	router.DELETE("/webhooks/:url", deregister)
+
 	static := httprouter.New()
 	static.ServeFiles("/*filepath", http.Dir("public"))
 	router.NotFound = static
 
+	log.Println("Listening on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func register(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	key := r.Header.Get("Authorization")
+
+	if key != sharedKey {
+		http.Error(w, http.StatusText(http.StatusForbidden), http.StatusForbidden)
+		return
+	}
+
+	url := ps.ByName("url")
+
+	if url == "" {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+	}
+
+	agents[url] = errorsBeforeDeregistration
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func deregister(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	key := r.Header.Get("Authorization")
+
+	if key != sharedKey {
+		http.Error(w, http.StatusText(http.StatusForbidden), http.StatusForbidden)
+		return
+	}
+
+	url := ps.ByName("url")
+
+	if url == "" {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+	}
+
+	delete(agents, url)
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func answer(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	queryValues := r.URL.Query()
+	agents, _ := json.Marshal(agents)
+
+	response := model.Response{
+		Name:     "foo",
+		Response: 1,
+		Delay:    50,
+	}
+
+	responses := []model.Response{response}
+
+	res, _ := json.Marshal(responses)
+	fmt.Fprintf(w, "question: %s, TTL: %s\nAgents: %s\nResponse: %s\n", queryValues.Get("q"), queryValues.Get("ttl"), agents, res)
 }
