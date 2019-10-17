3c4f7107fdede0e17981697dafb759264f89958a
JP Dillingham <jpdillingham@gmail.com>
2019-10-17 08:45:28 -0500

    tweaks

--- web/src/icosahedron/Icosahedron.js
+++ web/src/icosahedron/Icosahedron.js
@@ -5,10 +5,8 @@ class Icosahedron extends Component {
     render() {
         return (
             <div className="icosahedron">
-                <div className="textarea">
-                    <span className="text line">AS I</span>
-                    <span className="text line">SEE IT</span>
-                    <span className="text line">YES</span>
+                <div className="text">
+                Outlook not so good
                 </div>
             </div>
         );
