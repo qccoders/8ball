c71b819e577f604d313f9da9d044d3f855b90484
JP Dillingham <jpdillingham@gmail.com>
2019-10-17 22:31:23 -0500

    add animation

--- web/src/icosahedron/Icosahedron.js
+++ web/src/icosahedron/Icosahedron.js
@@ -1,13 +1,24 @@
 import React, { Component } from "react";
 import "./Icosahedron.css";
+import responses from "../responses"
 
 class Icosahedron extends Component {
+    state = { hidden: true }
+
+    componentDidUpdate(prevProps) {
+        if (this.props.refreshing !== prevProps.refreshing) {
+            this.setState({ hidden: this.props.refreshing })
+        }
+    }
+
     render() {
         return (
-            <div className="icosahedron">
-                <div className="text">
-                Outlook not so good
-                </div>
+            <div className="icosahedron-container">
+                {this.state.hidden || <div className="icosahedron fadeIn" style={{ opacity: this.state.opacity }}>
+                    <div className="text">
+                        {responses[this.props.response]}
+                    </div>
+                </div>}
             </div>
         );
     }
