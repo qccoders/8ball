9a35e38aea32e1d89934adb951397186752fc405
JP Dillingham <jpdillingham@gmail.com>
2019-10-17 22:30:24 -0500

    add refresh method, refresh button

--- web/src/app/App.js
+++ web/src/app/App.js
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import Icosahedron from "../icosahedron/Icosahedron";
 
-function App() {
-    return (
-        <div className="app">
-            <span className="window">
-                <Icosahedron result={0}/>
-            </span>
-        </div>
-    );
+class App extends Component {
+    state = { refreshing: false, response: 0 }
+
+    refresh = (e) => {
+        this.setState({ refreshing: true}, () => {
+            this.setState({ response: this.state.response + 1 }, () => {
+                this.setState({ refreshing: false })
+            })
+        })
+    }
+
+    render() {
+        return (
+            <div className="app">
+                <span className="window">
+                    <button onClick={this.refresh}>Next Response</button>
+                    <br/>
+                    <Icosahedron refreshing={this.state.refreshing} response={this.state.response}/>
+                </span>
+            </div>
+        );
+    }        
 }
 
 export default App;
