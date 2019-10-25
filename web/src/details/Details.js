55cdeaf2a853ecfcf047276991b4874fcc987d55
JP Dillingham <JP.Dillingham@rackspace.com>
2019-10-25 15:03:26 -0500

    split response components

--- web/src/details/Details.js
+++ web/src/details/Details.js
@@ -1,55 +1,19 @@
 import React, { Component } from "react";
 import "./Details.css";
 import responses from '../responses';
+import ResponseList from './ResponseList';
+import ResponseTable from './ResponseTable';
 
-import { Tab, List, Divider, Table } from 'semantic-ui-react';
+import { Tab, Divider } from 'semantic-ui-react';
 
 class Details extends Component {
-    responseList = (response, index = 0) => {
-        let details = `${responses[response.response]} (${response.response}${response.delay ? ` , ${response.delay}ms` : ''})`;
-
-        return (
-            <List inverted key={index}>
-                <List.Item>
-                    <List.Icon name={response.children ? 'share alternate' : 'circle'}/>
-                    <List.Content>
-                        <List.Header>{response.name}</List.Header>
-                        <List.Description>{details}</List.Description>
-                        {response.children && <List.List>
-                            {response.children.map((c, i) => this.responseList(c, i))}
-                        </List.List>}
-                    </List.Content>
-                </List.Item>
-            </List>
-        )
-    }
-
-    responseTable = () => (
-        <Table celled inverted className='response-table'>
-            <Table.Body>
-                <Table.Row>
-                    <Table.Cell className='response-table-label'>Response</Table.Cell>
-                    <Table.Cell>{`${responses[this.props.response]} (${this.props.response})`}</Table.Cell>
-                </Table.Row>
-                <Table.Row>
-                    <Table.Cell className='response-table-label'>Aggregation Strategy</Table.Cell>
-                    <Table.Cell>{this.props.strategy}</Table.Cell>
-                </Table.Row>
-                <Table.Row>
-                    <Table.Cell className='response-table-label'>Total Latency</Table.Cell>
-                    <Table.Cell>{this.props.delay}</Table.Cell>
-                </Table.Row>
-            </Table.Body>
-        </Table>
-    )
-    
     panes = () => [
         {
             menuItem: 'Details', 
             render: () => <Tab.Pane inverted className='details-tab'>
-                {this.responseTable()}
+                <ResponseTable response={this.props.response} strategy={this.props.strategy} delay={this.props.delay} responses={responses}/>
                 <Divider/>
-                {this.props.responses.map((r, i) => this.responseList(r, i))}
+                {this.props.responses.map((r, i) => <ResponseList key={i} index={i} response={r} responses={responses} />)}
             </Tab.Pane> 
         },
         { 
