97652ff939ec2e6713c6ab635f133a9ff9feb877
JP Dillingham <jpdillingham@gmail.com>
2019-10-28 07:37:03 -0500

    stub f# project from template

---
+++ agents/f#/WeatherForecast.fs
@@ -0,0 +1,11 @@
+namespace f_
+
+open System
+
+type WeatherForecast =
+    { Date: DateTime
+      TemperatureC: int
+      Summary: string }
+
+    member this.TemperatureF =
+        32 + (int (float this.TemperatureC / 0.5556))
