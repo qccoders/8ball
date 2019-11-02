73f577d0cda0598a9d52fed9ebc6eb4486479d24
JP Dillingham <jpdillingham@gmail.com>
2019-11-02 18:33:39 -0500

    add python/flask agent

---
+++ agents/python/agent.py
@@ -0,0 +1,15 @@
+import random
+from flask import Flask
+from flask import jsonify
+
+app = Flask(__name__)
+
+@app.route('/answer')
+def answer():
+    return jsonify(
+        name='Python+Flask',
+        response=random.randint(0,20)
+    )
+
+if __name__ == '__main__':
+    app.run()
