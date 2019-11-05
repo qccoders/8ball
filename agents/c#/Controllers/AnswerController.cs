d815aa2008bbc7394180657bc9c2500bdda4753a
JP Dillingham <jpdillingham@gmail.com>
2019-11-05 07:54:17 -0600

    rough in app

---
+++ agents/c#/Controllers/AnswerController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace c_.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AnswerController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var rng = new Random();
+
+            return Ok(new { 
+                Name = "C#",
+                Response = rng.Next(0,20),
+                Children = new [] {
+                    Name = "C♭",
+                    Response = rng.Next(0,20),
+                    Children = new [] {
+                        {
+                            Name = "C",
+                            Response = rng.Next(0,20)
+                        }
+                    }
+                }
+            });
+        }
+    }
+}
